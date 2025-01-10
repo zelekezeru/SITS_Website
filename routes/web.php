2d43c55f8388415a07cb897c4143564c768be26d
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2025-01-10 11:18:36 +0300

    logo
    logo 43 px

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/dashboard', [HomeController::class, 'dashboard'])->middleware(['aut
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
