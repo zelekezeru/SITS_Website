9098750c93436bd600b8df6ac23ceb2fc4c45245
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2025-04-03 03:24:29 -0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -39,14 +39,30 @@
             <div data-aos="zoom-in" data-aos-delay="400">
                 <h3 class="text-lg font-bold text-white mb-6">Newsletter</h3>
                 <p class="text-gray-400 text-sm mb-6">You can trust us. We only send promo offers.</p>
-                <form class="flex">
-                    <input type="email" placeholder="Your Email Address"
-                        class="w-full px-2 py-2 rounded-lg bg-gray-800 text-gray-300 border border-gray-700 focus:border-blue-500 focus:outline-none" />
-                    <button type="submit"
-                        class="p-2 bg-yellow-500 hover:bg-yellow-600 text-sm text-gray-900 font-bold rounded-lg transition">
-                        SUBSCRIBE
-                    </button>
+                <form class="max-w-md mx-auto">
+                    <label for="subscribe-email"
+                        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Email
+                        Subscription</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                <path
+                                    d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                                <path
+                                    d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                            </svg>
+                        </div>
+                        <input type="email" id="subscribe-email"
+                            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Enter your email..." required />
+                        <button type="submit"
+                            class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Subscribe
+                        </button>
+                    </div>
                 </form>
+
             </div>
         </div>
 
