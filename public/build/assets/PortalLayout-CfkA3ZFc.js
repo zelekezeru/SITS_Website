9756fae82edd8d03240ea5abf9ba4d23294ba0d1
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2026-07-30 16:16:15 +0300

    built

---
+++ public/build/assets/PortalLayout-CfkA3ZFc.js
@@ -0,0 +1,1 @@
+import{A as o}from"./AdminLayout-PGhi_ZIv.js";import{x as a,p as r,L as e,o as s}from"./app-BFPTfRME.js";const u={__name:"PortalLayout",setup(l){return(t,n)=>(s(),a(o,null,{default:r(()=>[e(t.$slots,"default")]),_:3}))}};export{u as _};
